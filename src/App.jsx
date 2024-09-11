b9c612a52018caf7391ca3edc03e04f0cf6ac1cb
Pradeep <pradee.sinivas@gmail.com>
2024-09-11 22:31:18 +0530

    kjac

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Form from './components/Form'
 const App = () => {
   return (
     <div className='hero-bg bg-scroll bg-cover h-screen font-serif'>
-      <div className="backdrop-blur-none bg-black/20 overflow-y-scroll h-screen no-scrollbar w-full p-8">
+      <div className="backdrop-blur-none bg-black/10 overflow-y-scroll h-screen no-scrollbar w-full p-8">
         <Navbar></Navbar>
         <Form></Form>
       </div>
